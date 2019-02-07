ee1c12ad0019ba9ed3a157d327eab4831eba04c7
DarkBird <josepokemon234@gmail.com>
2019-02-07 21:34:33 +0100

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace osuThumb
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.preview = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // preview
+            // 
+            this.preview.Location = new System.Drawing.Point(12, 12);
+            this.preview.Name = "preview";
+            this.preview.Size = new System.Drawing.Size(402, 253);
+            this.preview.TabIndex = 0;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.preview);
+            this.Name = "MainForm";
+            this.Text = "osu!Thumbnail Generator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel preview;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace osuThumb
+{
+    public partial class MainForm : Form
+    {
+        private string appdataPath = "";
+        private string osuFolder = "";
+        private string thumbFolder = "";
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            appdataPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            osuFolder = appdataPath + @"\osu!";
+
+            if (!Directory.Exists(osuFolder))
+            {
+                MessageBox.Show("ERROR: osu folder wasn't found", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                thumbFolder = osuFolder + @"\Data\bt";
+                if (!Directory.Exists(thumbFolder))
+                {
+                    MessageBox.Show("ERROR: osu's bt folder wasn't found", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+    }
+}
