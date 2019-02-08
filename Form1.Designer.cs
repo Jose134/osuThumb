792bf2ed3e66a8455728ebf66848e7625dd14828
DarkBird <josepokemon234@gmail.com>
2019-02-08 08:36:06 +0100

    First session of work
    OwO

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,31 +29,89 @@
         private void InitializeComponent()
         {
             this.preview = new System.Windows.Forms.Panel();
+            this.generateButton = new System.Windows.Forms.Button();
+            this.idBox = new System.Windows.Forms.TextBox();
+            this.idLabel = new System.Windows.Forms.Label();
+            this.accLabel = new System.Windows.Forms.Label();
+            this.accBox = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // preview
             // 
             this.preview.Location = new System.Drawing.Point(12, 12);
             this.preview.Name = "preview";
-            this.preview.Size = new System.Drawing.Size(402, 253);
+            this.preview.Size = new System.Drawing.Size(480, 360);
             this.preview.TabIndex = 0;
+            // 
+            // generateButton
+            // 
+            this.generateButton.Location = new System.Drawing.Point(498, 349);
+            this.generateButton.Name = "generateButton";
+            this.generateButton.Size = new System.Drawing.Size(290, 23);
+            this.generateButton.TabIndex = 1;
+            this.generateButton.Text = "Generate!";
+            this.generateButton.UseVisualStyleBackColor = true;
+            this.generateButton.Click += new System.EventHandler(this.generateButton_Click);
+            // 
+            // idBox
+            // 
+            this.idBox.Location = new System.Drawing.Point(578, 12);
+            this.idBox.Name = "idBox";
+            this.idBox.Size = new System.Drawing.Size(210, 20);
+            this.idBox.TabIndex = 2;
+            // 
+            // idLabel
+            // 
+            this.idLabel.AutoSize = true;
+            this.idLabel.Location = new System.Drawing.Point(498, 15);
+            this.idLabel.Name = "idLabel";
+            this.idLabel.Size = new System.Drawing.Size(74, 13);
+            this.idLabel.TabIndex = 3;
+            this.idLabel.Text = "Beatmapset id";
+            // 
+            // accLabel
+            // 
+            this.accLabel.AutoSize = true;
+            this.accLabel.Location = new System.Drawing.Point(520, 43);
+            this.accLabel.Name = "accLabel";
+            this.accLabel.Size = new System.Drawing.Size(52, 13);
+            this.accLabel.TabIndex = 4;
+            this.accLabel.Text = "Accuracy";
+            // 
+            // accBox
+            // 
+            this.accBox.Location = new System.Drawing.Point(578, 40);
+            this.accBox.Name = "accBox";
+            this.accBox.Size = new System.Drawing.Size(210, 20);
+            this.accBox.TabIndex = 5;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 390);
+            this.Controls.Add(this.accBox);
+            this.Controls.Add(this.accLabel);
+            this.Controls.Add(this.idLabel);
+            this.Controls.Add(this.idBox);
+            this.Controls.Add(this.generateButton);
             this.Controls.Add(this.preview);
             this.Name = "MainForm";
             this.Text = "osu!Thumbnail Generator";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Panel preview;
+        private System.Windows.Forms.Button generateButton;
+        private System.Windows.Forms.TextBox idBox;
+        private System.Windows.Forms.Label idLabel;
+        private System.Windows.Forms.Label accLabel;
+        private System.Windows.Forms.TextBox accBox;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -24,6 +24,7 @@ namespace osuThumb
 
         private void Form1_Load(object sender, EventArgs e)
         {
+
             appdataPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
             osuFolder = appdataPath + @"\osu!";
 
@@ -39,6 +40,32 @@ namespace osuThumb
                     MessageBox.Show("ERROR: osu's bt folder wasn't found", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
             }
+        }
+
+        private void generateButton_Click(object sender, EventArgs e)
+        {
+            string bgPath = thumbFolder + @"\" + idBox.Text + "l.jpg";
+            if (!File.Exists(bgPath))
+            {
+                MessageBox.Show("ERROR: Couldn't find background image, is id correct?", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            Image bgImg = Bitmap.FromFile(bgPath);
+
+            Graphics g = preview.CreateGraphics();
+            g.DrawImage(bgImg, new RectangleF(0, 15, preview.Width, preview.Height - 15));
+
+            SolidBrush semiBlackBursh = new SolidBrush(Color.FromArgb(191, 0, 0, 0));
+            g.FillRectangle(semiBlackBursh, new Rectangle(0, 0, preview.Width, preview.Height));
+
+            Font font = new Font("Arial", 24);
+            SolidBrush fontBrush = new SolidBrush(Color.FromArgb(255, 255, 255, 255));
+            
+            float f_acc = float.Parse(accBox.Text);
+            string acc = f_acc.ToString("0.00");
+
+            g.DrawString(acc + "%", font, fontBrush, new PointF(30, 30));
         }
     }
 }
